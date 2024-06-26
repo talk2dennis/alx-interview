213b8df125f7c28fd60f73c6a6a233bd2e6b7587
talk2dennis <adigwedennis@yahoo.com>
2024-06-26 19:08:28 +0100

    used git comprehension

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,12 +10,8 @@ def pascal_triangle(n):
     triangle = [[1]]
 
     for i in range(1, n):
-        p_row = triangle[i - 1]
-        new_row = [1]
+        p_row = triangle[-1]
 
-        # iterate over the previous row
-        for j in range(1, i):
-            new_row.append(p_row[j - 1] + p_row[j])
-        new_row.append(1)
+        new_row = [1] + [p_row[j - 1] + p_row[j] for j in range(1, i)] + [1]
         triangle.append(new_row)
     return triangle

781010cad8a9c5ff4a73a5d09b146fb61dee3c8f
talk2dennis <adigwedennis@yahoo.com>
2024-06-26 14:52:40 +0100

    completed the task pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+
+    for i in range(1, n):
+        p_row = triangle[i - 1]
+        new_row = [1]
+
+        # iterate over the previous row
+        for j in range(1, i):
+            new_row.append(p_row[j - 1] + p_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+    return triangle
+        

808b44e632fc390c619d2cd0df79563cfa827bb9
talk2dennis <adigwedennis@yahoo.com>
2024-07-02 13:36:40 +0100

    fixed for pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ a module to check if all boxes can unlock """
 
+
 def canUnlockAll(boxes):
-    """canUnlockAll
-	
-	Keyword arguments:
-	boxes -- list of list of integers
-	Return: true otherwise false
-	"""
+    """
+    canUnlockAll
+    Keyword arguments:
+        boxes -- list of list of integers
+    Return: true otherwise false
+    """
     n = len(boxes)
-    opened = [False] * n # set all to false (unopened)
-    opened[0] = True # set the first box to True (opened)
+    opened = [False] * n  # set all to false (unopened)
+    opened[0] = True  # set the first box to True (opened)
     stack = [0]
 
     while stack:
@@ -19,6 +20,5 @@ def canUnlockAll(boxes):
             if key < n and not opened[key]:
                 opened[key] = True
                 stack.append(key)
-    
+
     return all(opened)
-    

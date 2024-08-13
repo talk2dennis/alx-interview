2b649f6d17b2f6100a48c3e8129a6c044e87024d
talk2dennis <adigwedennis@yahoo.com>
2024-08-13 10:40:26 +0100

    updated the  algorithm

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,4 +7,12 @@
 def rotate_2d_matrix(matrix):
     """ Given an n x n 2D matrix, rotate it 90 degrees clockwise. """
     N = len(matrix[0])
-    return [[matrix[N - 1 - y][x] for y in range(N)] for x in range(N)]
+    for x in range(0, int(N / 2)):
+        for y in range(x, N - x - 1):
+            # Correct the assignments as follows
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[N - 1 - y][x]
+            matrix[N - 1 - y][x] = matrix[N - 1 - x][N - 1 - y]
+            matrix[N - 1 - x][N - 1 - y] = matrix[y][N - 1 - x]
+            matrix[y][N - 1 - x] = temp
+    return matrix
